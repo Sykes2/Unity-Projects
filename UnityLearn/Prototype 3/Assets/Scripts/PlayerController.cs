91acbf396b4bc00c8d6c8b0dce052eac037a8f95
Sykes2 <jelliott3@zagmail.gonzaga.edu>
2021-10-12 20:35:44 -0700

    First Prototype push
    Altogether push of the 4 prototypes I've made so far for Unity Learn

---
+++ UnityLearn/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public ParticleSystem fartCloud;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; 
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver){
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            playerAnim.SetTrigger("Jump_trig");
+            isOnGround = false;
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle")){
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            fartCloud.Stop();
+            gameOver = true;
+            Debug.Log("Game Over");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+        
+    }
+}
