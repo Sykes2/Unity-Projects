5895696aa232d417e3d54ff2e47be655a0886147
Sykes2 <jelliott3@zagmail.gonzaga.edu>
2021-11-01 20:51:29 -0700

    Prototype 5 time
    Time for Prototype the Vth

--- UnityLearn/Prototype 4/Assets/Scripts/Enemy.cs
+++ UnityLearn/Prototype 4/Assets/Scripts/Enemy.cs
@@ -12,14 +12,18 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
-        player  = GameObject.Find("Player");    
+        player = GameObject.Find("Player");    
     }
+
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if(transform.position.y < -10){Destroy(gameObject);}
     }
 
 
